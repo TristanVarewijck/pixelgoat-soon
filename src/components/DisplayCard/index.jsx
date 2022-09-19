1cad93e691541c6ffec13e7f25cec02e0b51e555
TristanVarewijck <tristanconnect4@outlook.com>
2022-09-19 23:55:04 +0200

    styling

---
+++ src/components/DisplayCard/index.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import "./DisplayCard.scss";
+import { DarkTheme } from "../../App";
+
+function DisplayCard() {
+  const darkMode = useContext(DarkTheme);
+  return (
+    <div class="display-card">
+      <div class="body body-display">
+        <div class="body-header">
+          <small>#12345</small>
+          <p>PixelGoat</p>
+        </div>
+        <div class="body-img-container">
+          <div class="body-img">
+            <img src="#" alt="nft collection profile image" />
+          </div>
+        </div>
+        <div class="body-content">
+          <div class="total">
+            <small>Total</small>
+            <p>6000</p>
+          </div>
+          <div class="mint-price">
+            <small>Mint</small>
+            <p>1.2 ETH</p>
+          </div>
+        </div>
+        <a href="#">Learn more about this collection</a>
+      </div>
+    </div>
+  );
+}
+
+export default DisplayCard;
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -2,24 +2,32 @@ import { useContext } from "react";
 import "./Hero.scss";
 import Subscribe from "../Subscribe";
 import { DarkTheme } from "../../App";
+import DisplayCard from "../DisplayCard";
 
 function Hero() {
   const darkMode = useContext(DarkTheme);
   return (
     <div className={`hero ${darkMode ? "darkmode" : ""}`}>
-      <small>
-        <img
-          src={`/assets/bulletpoint${darkMode ? "-white" : ""}.svg`}
-          alt="bulletpoint icon"
-        />
-        Pixelgoat Coming soon
-      </small>
-      <h1 className="h1">Get Notified before we Launch!</h1>
-      <h4>
-        Be first to know when a new NFT collection has been opened for Whitelist
-        Applications
-      </h4>
-      <Subscribe />
+      <div>
+        <small>
+          <img
+            src={`/assets/bulletpoint${darkMode ? "-white" : ""}.svg`}
+            alt="bulletpoint icon"
+          />
+          Pixelgoat Coming soon
+        </small>
+        <h1>Get Notified before we Launch!</h1>
+        <h4>
+          Be first to know when a new NFT collection has been opened for
+          Whitelist Applications
+        </h4>
+        <Subscribe />
+      </div>
+
+      <div className="cards-group">
+        <DisplayCard />
+        <DisplayCard />
+      </div>
     </div>
   );
 }
